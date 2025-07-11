d155058df21b27f9450289012038dcd0fd7c711c
Yuri-Kranholdt <iurikranholdt@gmail.com>
2025-07-11 12:55:53 -0300

    Update Program.cs
    Fundo branco

--- BarChart/Program.cs
+++ BarChart/Program.cs
@@ -36,7 +36,7 @@ public class BarChart
         this.hc = height - (padding * 2);
 
        
-        svg = $"<svg width=\"{width}\" height=\"{height}\" xmlns=\"http://www.w3.org/2000/svg\">\n";
+        svg = $"<svg width=\"{width}\" height=\"{height}\" xmlns=\"http://www.w3.org/2000/svg\" style=\"background-color: white\">\n";
     }
 
     double Plot_Y(double yc, double maxy)
